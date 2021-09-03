357464ffa73cbee022a853c8bd76974801896a44
Chun <jyc5842@psu.edu>
2021-09-02 20:08:58 -0400

    ic4

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/logo2.PNG`} alt="" />
+        <img src={`${PUBLIC_URL}/images/logo3.PNG`} alt="" />
       </Link>
       <header>
         <h2>Nittany Energy</h2>
