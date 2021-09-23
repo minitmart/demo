91afe50bc57b6bb2e1a4e8685842a314682e3a06
Jim <jiminbentley@gmail.com>
2021-09-23 05:54:06 -0400

    nittanny changes

--- src/data/routes.js
+++ src/data/routes.js
@@ -4,26 +4,6 @@ const routes = [
     label: 'Nittany Energy',
     path: '/',
   },
-  {
-    label: 'About',
-    path: '/about',
-  },
-  {
-    label: 'Resume',
-    path: '/resume',
-  },
-  {
-    label: 'Projects',
-    path: '/projects',
-  },
-  {
-    label: 'Stats',
-    path: '/stats',
-  },
-  {
-    label: 'Contact',
-    path: '/contact',
-  },
 ];
 
 export default routes;
